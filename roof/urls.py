6c9bef2019f836b16b8bbb644b4ab61ffae4ff4c
Sendr <sendr84@gmail.com>
2015-03-27 12:13:16 +0200

    Fixed urls

--- roof/urls.py
+++ roof/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('',
        url(r'^$', IndexPageView.as_view(), name='index'),
        url(r'^articles/$', ArticlesPageView.as_view(), name='articles'),
        url(r'^contact/$', ContactPageView.as_view(), name='contact'),
-       url(r'^article/(?P<slug>\w+)$', SelArtPageView.as_view(), name='sel_atr'),
+       url(r'^article/(?P<slug>[\w-]+)$', SelArtPageView.as_view(), name='sel_atr'),
        url(r'^tag_set/(?P<pk>\d+)$', TagSetView.as_view(), name='tag_set'),
 
 )
